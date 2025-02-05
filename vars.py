ea7e60030ae9e38e59995b8cb63817b2e4cb333b
Srikanthreddy140 <147992962+Srikanthreddy140@users.noreply.github.com>
2025-02-05 21:13:07 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
 from os import getenv
 class Settings:
-    API_ID  :int = int(getenv("API_ID", 0))
-    API_HASH  : str = str(getenv("API_HASH" , ""))
-    BOT_TOKEN : str = str(getenv("BOT_TOKEN" , ""))
-    MONGO_URI : str= str(getenv("MONGO_URL" , ""))
-    ADMIN : int= int(getenv("ADMIN" , 89)
+    API_ID  :int = int(getenv("API_ID", 20154522))
+    API_HASH  : str = str(getenv("API_HASH" , "ec1b720c2e0035a310d47ebd5655b676"))
+    BOT_TOKEN : str = str(getenv("BOT_TOKEN" , "7887189932:AAE2lr7LQDk2zsrYcYjmRyuWFAAgwT6oLF8"))
+    MONGO_URI : str= str(getenv("MONGO_URL" , "mongodb+srv://royalsrikanth140:n0O97ntpssfJWUjX@cluster0.788gy1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+    ADMIN : int= int(getenv("ADMIN" , 7003079691)
 )
 settings = Settings()
